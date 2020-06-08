f23e619637b1cc840e44f4c1ceaa15f6ccf9e6a5
Zhydovlenko <you@example.com>
2020-06-08 15:24:11 +0300

    Updates

---
+++ precache-manifest.fbe988cbe0ea658523319f8060aac49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4bb8960d3ad7423fcd8554868e661e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "a486d3f57a464aa933b6",
+    "url": "/goit-react-hw-02-feedback/static/css/main.2a7df8c3.chunk.css"
+  },
+  {
+    "revision": "e3683ac84d5203e922eb",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c454d4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c454d4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a486d3f57a464aa933b6",
+    "url": "/goit-react-hw-02-feedback/static/js/main.47599dd9.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
